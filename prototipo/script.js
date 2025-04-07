9dc7a5c519f96d0f8dc661ff87d8ba70400bbfd0
Yasmin Limas <131016914+imyayamin@users.noreply.github.com>
2025-04-07 15:51:42 -0300

    07-04-2025

---
+++ prototipo/script.js
@@ -0,0 +1,5 @@
+// função para abrir a pagina de login e cadastra-se ao clilcar na imagem do nav
+ function toggleMenu() {
+    const menu = document.getElementById("userMenu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+  }
